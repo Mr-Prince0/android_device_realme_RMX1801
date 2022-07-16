1b6bf8850fe83a9ce3e2cc2d204813848db98a66
Sanji12335 <109226382+Sanji12335@users.noreply.github.com>
2022-07-16 14:31:08 +0530

    Update arrow_RMX1801.mk

--- arrow_RMX1801.mk
+++ arrow_RMX1801.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/blaze/config/common.mk)
 $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 # Inherit from Realme RMX1801
@@ -28,13 +28,19 @@ $(call inherit-product, device/realme/RMX1801/device.mk)
 # Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-PRODUCT_NAME := arrow_RMX1801
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := OFFICIAL
+BLAZE_MAINTAINER := Luffy
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+#Device identifier
+PRODUCT_NAME := blaze_RMX1801
 PRODUCT_DEVICE := RMX1801
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := Realme 2 Pro
 PRODUCT_ARCH := arm64
-DEVICE_MAINTAINER := Baibhab
+
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 TARGET_VENDOR_PRODUCT_NAME := RMX1801
